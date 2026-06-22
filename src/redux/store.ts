f723783fcc40a0ff6c31e06ccddebdedcfd26837
Faysal2123 <foysal2005n1@gmail.com>
2026-06-22 20:04:59 +0600

    feat: configure API slice reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../redux/cart/cartSlice';
 import productReducer from '../redux/product/productSlice'
+import {api} from '../redux/api/apiSlice'
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    product: productReducer
+    product: productReducer,
+    [api.reducerPath]:api.reducer
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
