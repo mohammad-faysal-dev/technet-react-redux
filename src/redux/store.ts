01f871eb1993cd087e98f0f0de9787d39ed7c3ea
Faysal2123 <foysal2005n1@gmail.com>
2026-06-22 20:54:27 +0600

    feat: integrate RTK Query middleware and fetch products

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../redux/cart/cartSlice';
-import productReducer from '../redux/product/productSlice'
-import {api} from '../redux/api/apiSlice'
+import productReducer from '../redux/product/productSlice';
+import { api } from '../redux/api/apiSlice';
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducer,
-    [api.reducerPath]:api.reducer
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 export type RootState = ReturnType<typeof store.getState>;
 
